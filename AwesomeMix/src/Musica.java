ce726c1fdfe83881571aee4594adaa6296ceb22a
Thamiris Coelho <thamycoelho10@gmail.com>
2018-04-25 14:01:50 -0300

    enum estilos musicais e classe musica

--- AwesomeMix/src/Musica.java
+++ AwesomeMix/src/Musica.java
@@ -7,14 +7,26 @@ public class Musica extends Album{
 	private double avaliacao;
 	
 	//Metodo Construtor
-	public Musica(){
-		super();
-		this.id=qtdMusicasTotal;
-		this.nomeMusica=" ";
-		this.tempoDuracao=0.0;
-		this.avaliacao=0.0;
+	//Recebe alem dos atributos da musica os atributos para criacao do album
+	public Musica(String nomeAlbum, String artista, int anoLancamento, double avaliacaoAlbum, String nomeMusica, double tempoDuracao, double avaliacaoMusica){
+		super(nomeAlbum, artista, anoLancamento, avaliacaoAlbum);
+		this.id = qtdMusicasTotal;
+		this.nomeMusica = nomeMusica;
+		this.tempoDuracao = tempoDuracao;
+		this.avaliacao = avaliacaoMusica;
 		qtdMusicasTotal++;
 	}
+	
+	//O album ja existe e so precisamos criar a musica que pertence ao album
+	public Musica(String nomeMusica, double tempoDuracao, double avaliacaoMusica){
+		super();
+		this.id = qtdMusicasTotal;
+		this.nomeMusica = nomeMusica;
+		this.tempoDuracao = tempoDuracao;
+		this.avaliacao = avaliacaoMusica;
+		qtdMusicasTotal++;
+	}
+	
 	//Metodos Getters e Setters da classe Musica
 	public static int getQtdMusicasTotal() {
 		return qtdMusicasTotal;
@@ -47,5 +59,16 @@ public class Musica extends Album{
 	public int getId() {
 		return id;
 	}	
+	
+	public void atribuirNota(double avaliacao) {
+		this.avaliacao = (this.avaliacao + avaliacao)/2;
+	}
+
+	@Override
+	public String toString() {
+		return "Musica [id=" + id + ", nomeMusica=" + nomeMusica + ", tempoDuracao=" + tempoDuracao + ", avaliacao="
+				+ avaliacao + "]";
+	}
+	
 	
 }
